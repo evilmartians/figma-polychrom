d0ab00a14594a5cf1d00536dbfcc7da4a035da7a
Yuri Mikhin <urasergeevich@mail.ru>
2023-09-12 14:34:51 +0300

    Clear apcach types.

--- src/types/apcach.d.ts
+++ src/types/apcach.d.ts
@@ -13,11 +13,6 @@ declare module 'apcach' {
     lightness: number;
   }
 
-  interface Antagonist {
-    bg?: string;
-    fg?: string;
-  }
-
   function apcach(
     contrast: ContrastConfig | number,
     chroma:
@@ -31,70 +26,14 @@ declare module 'apcach' {
     alpha?: number
   ): ApcachColor;
 
-  function cssToApcach(color: string, antagonist: Antagonist): ApcachColor;
-
-  function crToBg(bgColor: string, cr: number): ContrastConfig;
-
   function crTo(bgColor: string, cr: number): ContrastConfig;
 
-  function crToBlack(cr: number): ContrastConfig;
-
   function crToFg(fgColor: string, cr: number): ContrastConfig;
-
-  function crToFgWhite(cr: number): ContrastConfig;
-
-  function crToFgBlack(cr: number): ContrastConfig;
-
-  function setContrast(
-    colorInApcach: ApcachColor,
-    cr: ((currentCr: number) => number) | number
-  ): ApcachColor;
-
-  function setChroma(
-    colorInApcach: ApcachColor,
-    c: ((currentChroma: number) => number) | number
-  ): ApcachColor;
-
-  function setHue(
-    colorInApcach: ApcachColor,
-    h: ((currentHue: number) => number) | number
-  ): ApcachColor;
-
-  function maxChroma(
-    chromaCap?: number
-  ): (
-    contrastConfig: ContrastConfig,
-    hue: number,
-    alpha: number
-  ) => ApcachColor;
 
   function apcachToCss(
     color: ApcachColor,
     format: 'figma-p3' | 'hex' | 'oklch' | 'p3' | 'rgb'
   ): string;
 
-  function p3contrast(
-    fgColorInCssFormat: string,
-    bgColorInCssFormat: string
-  ): number;
-
-  function inP3(color: ApcachColor): boolean;
-
-  export {
-    apcach,
-    apcachToCss,
-    crTo,
-    crToBg,
-    crToBlack,
-    crToFg,
-    crToFgBlack,
-    crToFgWhite,
-    cssToApcach,
-    inP3,
-    maxChroma,
-    p3contrast,
-    setChroma,
-    setContrast,
-    setHue,
-  };
+  export { apcach, apcachToCss, crTo, crToFg };
 }
