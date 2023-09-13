aaefa98f83daf066928c51345edc799004369e4f
Yuri Mikhin <urasergeevich@mail.ru>
2023-09-14 00:13:45 +0300

    Add tests for node-has-fills and traverse-to-root.

---
+++ src/utils/figma/specs/node-has-fills.spec.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from 'vitest';
+
+import { nodeHasFills } from '../node-has-fills.ts';
+
+describe('nodeHasFills', () => {
+  test('should return false if node has no fills', () => {
+    const node = {} as unknown as SceneNode;
+
+    expect(nodeHasFills(node)).toBe(false);
+  });
+
+  test('should return false if node has fills but none of them are visible', () => {
+    const node = {
+      fills: [
+        { color: { b: 0, g: 0, r: 1 }, type: 'SOLID', visible: false },
+        { color: { b: 0, g: 1, r: 0 }, type: 'SOLID', visible: false },
+      ],
+    } as unknown as SceneNode;
+
+    expect(nodeHasFills(node)).toBe(false);
+  });
+
+  test('should return true if node has fills and at least one of them is visible', () => {
+    const node = {
+      fills: [
+        { color: { b: 0, g: 0, r: 1 }, type: 'SOLID', visible: false },
+        { color: { b: 0, g: 1, r: 0 }, type: 'SOLID', visible: true },
+      ],
+    } as unknown as SceneNode;
+
+    expect(nodeHasFills(node)).toBe(true);
+  });
+});
---
+++ src/utils/figma/specs/traverse-to-root.spec.ts
@@ -0,0 +1,47 @@
+import { describe, expect, test } from 'vitest';
+
+import { traverseToRoot } from '../traverse-to-root.ts';
+
+describe('traverseToRoot', () => {
+  test('should return an array with only the node if it has no parent', () => {
+    const node = {
+      id: '1',
+      parent: null,
+    } as unknown as SceneNode;
+
+    expect(traverseToRoot(node)).toEqual([node]);
+  });
+
+  test('should return an array with the node and its parent if it has one parent', () => {
+    const parent = {
+      id: '1',
+      parent: null,
+    } as unknown as SceneNode;
+
+    const node: SceneNode = {
+      id: '2',
+      parent,
+    } as unknown as SceneNode;
+
+    expect(traverseToRoot(node)).toEqual([parent, node]);
+  });
+
+  test('should return an array with the node and all its parents if it has multiple parents', () => {
+    const grandparent = {
+      id: '1',
+      parent: null,
+    } as unknown as SceneNode;
+
+    const parent = {
+      id: '2',
+      parent: grandparent,
+    } as unknown as SceneNode;
+
+    const node = {
+      id: '3',
+      parent,
+    } as unknown as SceneNode;
+
+    expect(traverseToRoot(node)).toEqual([grandparent, parent, node]);
+  });
+});
