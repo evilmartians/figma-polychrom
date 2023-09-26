6699096ade039449ffe71ba59a2094408f15b616
Yuri Mikhin <urasergeevich@mail.ru>
2023-09-26 21:35:18 +0300

    Remove unused types.

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,11 +1,3 @@
-declare module '*.svg' {
-  import React = require('react');
-  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  // eslint-disable-next-line import/no-default-export
-  export default src;
-}
-
 declare module 'apca-w3' {
   export {
     adobeRGBtoY,
