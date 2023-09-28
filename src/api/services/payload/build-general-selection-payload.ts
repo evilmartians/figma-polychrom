4303320ade44f2c40e6c62f005dd17998ed00879
Yuri Mikhin <urasergeevich@mail.ru>
2023-09-28 17:56:19 +0300

    Remove unused notEmpty.

--- src/api/services/payload/build-general-selection-payload.ts
+++ src/api/services/payload/build-general-selection-payload.ts
@@ -1,17 +1,14 @@
 import { getIntersectingNodes } from '~api/services/figma/intersections/get-intersecting-nodes.ts';
 import { createFigmaNode } from '~api/services/figma/nodes/create-figma-node.ts';
 import { type SelectionChangeMessage } from '~types/messages.ts';
-import { notEmpty } from '~utils/not-empty.ts';
 
 export const buildGeneralSelectionPayload = (
   selection: readonly SceneNode[]
 ): SelectionChangeMessage => {
-  const selectedNodePairs = selection
-    .map((selectedNode) => ({
-      intersectingNodes: getIntersectingNodes(selectedNode),
-      selectedNode: createFigmaNode(selectedNode),
-    }))
-    .filter(notEmpty);
+  const selectedNodePairs = selection.map((selectedNode) => ({
+    intersectingNodes: getIntersectingNodes(selectedNode),
+    selectedNode: createFigmaNode(selectedNode),
+  }));
 
   return {
     colorSpace: figma.root.documentColorProfile,
