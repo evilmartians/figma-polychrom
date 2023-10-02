98da00f284ce3d1b42b1eeb3307ebfa59d569446
Yuri Mikhin <urasergeevich@mail.ru>
2023-10-02 14:30:03 +0300

    Fix siblings.

--- src/api/services/figma/intersections/traverse-and-check-intersections.ts
+++ src/api/services/figma/intersections/traverse-and-check-intersections.ts
@@ -1,5 +1,22 @@
 import { areNodesIntersecting } from './are-nodes-intersecting.ts';
 
+export const getSiblingsBefore = (
+  targetNode: SceneNode,
+  allNodes: readonly SceneNode[]
+): SceneNode[] => {
+  const targetIndex = allNodes.indexOf(targetNode);
+  return targetIndex === -1 ? [] : allNodes.slice(0, targetIndex);
+};
+
+const ifSelectedNodeIsChild = (
+  node: SceneNode,
+  selectedNode: SceneNode
+): boolean => {
+  return (
+    'children' in node && node.children.some((n) => n.id === selectedNode.id)
+  );
+};
+
 // will return all nodes that are intersecting with selected node starting from depth a.k.a. neighboring nodes to the root a.k.a. PageNode
 export const traverseAndCheckIntersections = (
   nodes: SceneNode[],
@@ -11,11 +28,21 @@ export const traverseAndCheckIntersections = (
       accumulator.push(node);
 
       if ('children' in node && node.children.length > 0) {
-        traverseAndCheckIntersections(
-          Array.from(node.children),
-          selectedNode,
-          accumulator
-        );
+        if (ifSelectedNodeIsChild(node, selectedNode)) {
+          const siblingsBefore = getSiblingsBefore(selectedNode, node.children);
+
+          traverseAndCheckIntersections(
+            siblingsBefore,
+            selectedNode,
+            accumulator
+          );
+        } else {
+          traverseAndCheckIntersections(
+            Array.from(node.children),
+            selectedNode,
+            accumulator
+          );
+        }
       }
     }
   });
